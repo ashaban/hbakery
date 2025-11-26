6d3a9d3b50922968936033b4b51face92628d3af
Ally Shaban <allyshaban5@gmail.com>
2025-11-26 19:35:37 +0300

    add sales report

---
+++ backend/gui/assets/Logout-DHppW2Gc.js
@@ -0,0 +1,1 @@
+import{_ as t,q as o,t as s}from"./index-BkHo5HJJ.js";import{V as r}from"./VContainer-DZ_OfYr6.js";const e={mounted(){this.$store.dispatch("logout")}};function a(c,n,i,p,_,d){return s(),o(r,{"grid-list-xs":""})}const m=t(e,[["render",a]]);export{m as default};
