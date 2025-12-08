1b44f3b466bf7bc71ce5a07cda8d2248d043b6a0
Ally Shaban <allyshaban5@gmail.com>
2025-12-08 15:08:51 +0300

    properly computed available stock during editing

---
+++ backend/gui/assets/Logout-CChDPUNs.js
@@ -0,0 +1,1 @@
+import{_ as t,q as o,t as s}from"./index-AufZmc18.js";import{V as r}from"./VContainer-B7y45AGF.js";const e={mounted(){this.$store.dispatch("logout")}};function a(c,n,i,p,_,d){return s(),o(r,{"grid-list-xs":""})}const m=t(e,[["render",a]]);export{m as default};
